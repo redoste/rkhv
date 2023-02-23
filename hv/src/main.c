dea36cd4f2924eec35ebfa6bc325a551b7d3cd2a
redoste <redoste@redoste.xyz>
2023-02-24 00:08:36 +0100

    hv/main : Mark hvmain as noreturn

--- hv/src/main.c
+++ hv/src/main.c
@@ -7,7 +7,9 @@
 #include "panic.h"
 #include "segments.h"
 
-__attribute__((section(".text.entry"))) void hvmain(chainload_page_t* chainload_page) {
+__attribute__((section(".text.entry")))
+__attribute__((noreturn)) void
+hvmain(chainload_page_t* chainload_page) {
 	LOG("rkhv loaded - git:" RKHV_HASH " - chainload_page @ %p", (void*)chainload_page);
 	segments_setup();
 	interrupts_setup();
