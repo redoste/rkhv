1500b2e47dd66fd02d85075d183ca142e94143c6
redoste <redoste@redoste.xyz>
2023-06-11 17:20:31 +0200

    hv/vm : Factorize guest paddr to host vaddr in guest id paging setup

--- hv/src/vmm/vm_guest_paging.c
+++ hv/src/vmm/vm_guest_paging.c
@@ -9,37 +9,35 @@
 #include "vm_guest_paging.h"
 #include "vmx_ept.h"
 
+#define EPT_GET_HOSTVA(guestpa, hostva)                                                                      \
+	do {                                                                                                 \
+		uintptr_t hostpa;                                                                            \
+		if (!vmx_ept_get_host_physical_address(vm, guestpa, &hostpa)) {                              \
+			PANIC("Unable to walk EPT to get host physical address from guest physical address " \
+			      "during guest identity paging setup");                                         \
+		}                                                                                            \
+		hostva = P2V_IDENTITY_MAP(hostpa);                                                           \
+	} while (0)
+
 uintptr_t vm_guest_paging_setup_identity(vm_t* vm) {
 	if (vm->guest_physical_pages == 0) {
 		PANIC("Unable to setup guest identity paging on a VM without guest physical pages allocated");
 	}
-	const char* unable_to_walk_ept_panic_str =
-		"Unable to walk EPT to get host physical address from guest physical address "
-		"during guest identity paging setup";
 
 	// NOTE : As explained in paging_map_physical in boot/src/paging.c : We use 2MiB pages for compatibility
 
 	uintptr_t pml4_guestpa = (vm->guest_physical_pages - 1) * PAGE_SIZE;
-	uintptr_t pml4_hostpa;
-	if (!vmx_ept_get_host_physical_address(vm, pml4_guestpa, &pml4_hostpa)) {
-		PANIC(unable_to_walk_ept_panic_str);
-	}
-	uint64_t* pml4_hostva = P2V_IDENTITY_MAP(pml4_hostpa);
+	uint64_t* pml4_hostva;
+	EPT_GET_HOSTVA(pml4_guestpa, pml4_hostva);
 
 	uintptr_t pdpt_guestpa = pml4_guestpa - PAGE_SIZE;
-	uintptr_t pdpt_hostpa;
-	if (!vmx_ept_get_host_physical_address(vm, pdpt_guestpa, &pdpt_hostpa)) {
-		PANIC(unable_to_walk_ept_panic_str);
-	}
-	uint64_t* pdpt_hostva = P2V_IDENTITY_MAP(pdpt_hostpa);
+	uint64_t* pdpt_hostva;
+	EPT_GET_HOSTVA(pdpt_guestpa, pdpt_hostva);
 
 	for (size_t pdpt_index = 0; pdpt_index < PAGE_TABLE_ENTRIES; pdpt_index++) {
 		uintptr_t pd_guestpa = pdpt_guestpa - ((pdpt_index + 1) * PAGE_SIZE);
-		uintptr_t pd_hostpa;
-		if (!vmx_ept_get_host_physical_address(vm, pd_guestpa, &pd_hostpa)) {
-			PANIC(unable_to_walk_ept_panic_str);
-		}
-		uint64_t* pd_hostva = P2V_IDENTITY_MAP(pd_hostpa);
+		uint64_t* pd_hostva;
+		EPT_GET_HOSTVA(pd_guestpa, pd_hostva);
 
 		for (size_t pd_index = 0; pd_index < PAGE_TABLE_ENTRIES; pd_index++) {
 			uintptr_t pde_physical_address_page = ((pdpt_index * PAGE_TABLE_ENTRIES) + pd_index) << LINEAR_ADDRESS_D_SHIFT;
@@ -56,3 +54,4 @@ uintptr_t vm_guest_paging_setup_identity(vm_t* vm) {
 						  (pdpt_guestpa & PML4E_PHYSICAL_ADDRESS_PDPT);
 	return pml4_guestpa;
 }
+#undef EPT_GET_HOSTVA
