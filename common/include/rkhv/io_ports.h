120515addaf0925a91a32c630b9b1a23deb60656
redoste <redoste@redoste.xyz>
2023-06-11 21:36:29 +0200

    common/io : Mark I/O ports inline asm as volatile
    clang with `-O2` optimized away 2 consecutive `in` on the same port
    as if they returned the same value.

--- common/include/rkhv/io_ports.h
+++ common/include/rkhv/io_ports.h
@@ -5,16 +5,16 @@
 
 static inline uint8_t inb(uint16_t port) {
 	uint8_t ret;
-	asm("inb %1, %0"
-	    : "=a"(ret)
-	    : "d"(port));
+	asm volatile("inb %1, %0"
+		     : "=a"(ret)
+		     : "d"(port));
 	return ret;
 }
 
 static inline void outb(uint16_t port, uint8_t value) {
-	asm("outb %0, %1"
-	    :
-	    : "a"(value), "d"(port));
+	asm volatile("outb %0, %1"
+		     :
+		     : "a"(value), "d"(port));
 }
 
 #endif
