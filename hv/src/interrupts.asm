4cc92ade745c2a8156ab61d7188dcc3dcb4fec17
redoste <redoste@redoste.xyz>
2023-07-25 04:36:48 +0200

    hv/interrupts : Backup {x87,SSE,AVX} registers on interrupt using XSAVE

--- hv/src/interrupts.asm
+++ hv/src/interrupts.asm
@@ -2,6 +2,7 @@ bits 64
 section .text
 
 extern interrupts_handler
+extern xsave_area_size
 
 %macro isr_backup_regs 0
 	; Pushed by the interrupt :
@@ -27,7 +28,6 @@ extern interrupts_handler
 	push r14
 	push r15
 	; NOTE : For now we assume data segment selectors keep their default values
-	; TODO : We should probably backup XMM registers
 %endmacro
 
 %macro isr_restore_regs 0
@@ -54,10 +54,25 @@ isr_stub_%1:
 	mov rdi, %1
 	mov rsi, rsp
 
+	mov rbx, rsp
+	sub rsp, [rel xsave_area_size]
+	and rsp, 0xffffffffffffffc0 ; The XSAVE area must be 64-byte aligned
+
+	xor eax, eax
+	not eax
+	mov edx, eax
+	xsave [rsp]
+
 	mov rbp, rsp
 	and rsp, 0xfffffffffffffff0 ; Clang expect 16-byte aligned stack for doing its XMM shenanigans
 	call interrupts_handler
 	mov rsp, rbp
+
+	xor eax, eax
+	not eax
+	mov edx, eax
+	xrstor [rsp]
+	mov rsp, rbx
 
 	isr_restore_regs
 	iretq
