07bd529f567c0ab4f472a3b842d72c944ddf38d8
redoste <redoste@redoste.xyz>
2023-07-25 22:13:54 +0200

    hv/vmx : Reset {x87,SSE,AVX} to their initial state on vmlaunch

--- hv/src/vmm/vmx_instructions.asm
+++ hv/src/vmm/vmx_instructions.asm
@@ -52,6 +52,8 @@ vmx_vmwrite:
 	vmwrite rdi, rsi
 	jmp vmx_error_check
 
+extern xsave_area_size
+
 global vmx_vmlaunch
 vmx_vmlaunch:
 	; We backup the callee-saved registers in case the vmlaunch fails
@@ -62,22 +64,42 @@ vmx_vmlaunch:
 	push r14
 	push r15
 
+	mov rbx, rdi
+
+	mov rcx, [rel xsave_area_size]
+	mov rbp, rsp
+	sub rsp, rcx
+	and rsp, 0xffffffffffffffc0 ; The XSAVE area must be 64-byte aligned
+
+	xor eax, eax
+	mov rdi, rsp
+	rep stosb
+
+	; We xrstor an empty XSAVE area to reset x87 and the SIMD extensions to their initial state
+	; See Intel Manual Volume 1 : Chapter 13.8 : Operation of XRSTOR
+	; > If XSTATE_BV[i] = 0, the state component is set to its initial configuration.
+	; Here XSTATE_BV = 0 since we zeroed all the XSAVE area
+	not eax
+	mov edx, eax
+	xrstor [rsp]
+	mov rsp, rbp
+
 	; We received a vmx_initial_gpr_state_t as the first argument
-	mov r15, [rdi +  0*8]
-	mov r14, [rdi +  1*8]
-	mov r13, [rdi +  2*8]
-	mov r12, [rdi +  3*8]
-	mov r11, [rdi +  4*8]
-	mov r10, [rdi +  5*8]
-	mov r9,  [rdi +  6*8]
-	mov r8,  [rdi +  7*8]
-	mov rsi, [rdi +  9*8]
-	mov rbp, [rdi + 10*8]
-	mov rdx, [rdi + 11*8]
-	mov rcx, [rdi + 12*8]
-	mov rbx, [rdi + 13*8]
-	mov rax, [rdi + 14*8]
-	mov rdi, [rdi +  8*8]
+	mov r15, [rbx +  0*8]
+	mov r14, [rbx +  1*8]
+	mov r13, [rbx +  2*8]
+	mov r12, [rbx +  3*8]
+	mov r11, [rbx +  4*8]
+	mov r10, [rbx +  5*8]
+	mov r9,  [rbx +  6*8]
+	mov r8,  [rbx +  7*8]
+	mov rdi, [rbx +  8*8]
+	mov rsi, [rbx +  9*8]
+	mov rbp, [rbx + 10*8]
+	mov rdx, [rbx + 11*8]
+	mov rcx, [rbx + 12*8]
+	mov rax, [rbx + 14*8]
+	mov rbx, [rbx + 13*8]
 
 	vmlaunch
 
