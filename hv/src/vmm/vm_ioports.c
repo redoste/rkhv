2ee68aecd7042782ae5f3d110328844f30c8896d
redoste <redoste@redoste.xyz>
2023-08-11 10:19:37 +0200

    hv/vm : Ignore I/O operations on unexpected ports

--- hv/src/vmm/vm_ioports.c
+++ hv/src/vmm/vm_ioports.c
@@ -11,20 +11,19 @@
 
 // TODO : A {,d}word I/O operation is equivalent to {two,four} byte I/O operations on consecutive ports
 
-#define VM_OUT_DEVICE_LOOKUP(size, type)                                                                             \
-	void vm_out##size(vm_t* vm, uint16_t port, type value) {                                                     \
-		for (vm_device_t* device_iter = vm->devices; device_iter != NULL; device_iter = device_iter->next) { \
-			for (size_t i = 0; i < device_iter->ports_len; i++) {                                        \
-				if (port == device_iter->ports[i] &&                                                 \
-				    device_iter->out##size##_handler != NULL) {                                      \
-					device_iter->out##size##_handler(vm, device_iter->device_data, port, value); \
-					return;                                                                      \
-				}                                                                                    \
-			}                                                                                            \
-		}                                                                                                    \
-                                                                                                                     \
-		LOG("out" #size " \"%s\" port=%xpw value=%xp" #size, vm->name, port, value);                         \
-		PANIC("unsupported I/O port");                                                                       \
+#define VM_OUT_DEVICE_LOOKUP(size, type)                                                                               \
+	void vm_out##size(vm_t* vm, uint16_t port, type value) {                                                       \
+		for (vm_device_t* device_iter = vm->devices; device_iter != NULL; device_iter = device_iter->next) {   \
+			for (size_t i = 0; i < device_iter->ports_len; i++) {                                          \
+				if (port == device_iter->ports[i] &&                                                   \
+				    device_iter->out##size##_handler != NULL) {                                        \
+					device_iter->out##size##_handler(vm, device_iter->device_data, port, value);   \
+					return;                                                                        \
+				}                                                                                      \
+			}                                                                                              \
+		}                                                                                                      \
+                                                                                                                       \
+		LOG("\"%s\" : Unexpected out" #size " port=%xpw value=%xp" #size " : ignored", vm->name, port, value); \
 	}
 
 VM_OUT_DEVICE_LOOKUP(b, uint8_t)
@@ -42,8 +41,8 @@ VM_OUT_DEVICE_LOOKUP(d, uint32_t)
 			}                                                                                            \
 		}                                                                                                    \
                                                                                                                      \
-		LOG("in" #size " \"%s\" port=%xpw", vm->name, port);                                                 \
-		PANIC("unsupported I/O port");                                                                       \
+		LOG("\"%s\" : Unexpected in" #size " port=%xpw : ignored", vm->name, port);                          \
+		return 0;                                                                                            \
 	}
 
 VM_IN_DEVICE_LOOKUP(b, uint8_t)
